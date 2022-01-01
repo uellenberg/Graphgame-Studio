c01d05d67d08436072d7b1cb7796e4b1f88118f7
uellenberg <JonahUellenberg@hotmail.com>
2021-12-31 17:32:18 -0800

    Add file dependencies

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const { FilerWebpackPlugin } = require('filer/webpack')
 
 module.exports = withPWA({
     pwa: {
         dest: 'public',
         runtimeCaching,
     },
+    plugins: [
+        new FilerWebpackPlugin(),
+    ],
 })
