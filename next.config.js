290306edbb7aa7a7c3defd46016b5b678a87b695
uellenberg <JonahUellenberg@hotmail.com>
2022-01-01 20:59:32 -0800

    Fix production build

--- next.config.js
+++ next.config.js
@@ -23,6 +23,33 @@ module.exports = withPWA({
 
         config.plugins.push(new webpack.ProvidePlugin({ BrowserFS: 'bfsGlobal', process: 'processGlobal', Buffer: 'bufferGlobal' }))
 
+        config.optimization.splitChunks = {
+            chunks: 'async',
+            minSize: 20000,
+            minRemainingSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: false,
+                default: false,
+                framework: {
+                    chunks: 'all',
+                    name: 'framework',
+                    test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
+                    priority: 40,
+                    enforce: true,
+                },
+                commons: {
+                    name: 'commons',
+                    chunks: 'initial',
+                    minChunks: 20,
+                    priority: 20,
+                },
+            },
+        }
+
         return config
     },
 })
