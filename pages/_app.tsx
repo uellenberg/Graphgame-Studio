44b0526d305a64bf535a6ccd49935f425c61f82c
uellenberg <JonahUellenberg@hotmail.com>
2022-01-04 20:43:18 -0800

    Configure PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,11 +23,16 @@ const App = ({Component, pageProps}: {Component: any, pageProps: any}) => {
                     name="viewport"
                     content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
                 />
-                <meta name="description" content="Description" />
-                <meta name="keywords" content="Keywords" />
+                <meta name="description" content="An IDE for Graphgame, a game engine for writing games and animations in Desmos." />
+                <meta name="keywords" content="graph,game,graphgame,desmos,game,engine,logimat" />
                 <title>Graphgame Studio</title>
 
                 <link rel="manifest" href="/manifest.json" />
+                <link
+                    href="/favicon.ico"
+                    rel="shortcut icon"
+                    type="image/x-icon"
+                />
                 <link
                     href="/icons/favicon-16x16.png"
                     rel="icon"
@@ -40,8 +45,8 @@ const App = ({Component, pageProps}: {Component: any, pageProps: any}) => {
                     type="image/png"
                     sizes="32x32"
                 />
-                <link rel="apple-touch-icon" href="/apple-icon.png"/>
-                <meta name="theme-color" content="#317EFB" />
+                <link rel="apple-touch-icon" href="/icons/icon-180x180.png"/>
+                <meta name="theme-color" content="#181717" />
             </Head>
             <ThemeProvider theme={theme}>
                 <Component {...pageProps} />
