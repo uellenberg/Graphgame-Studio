e213f8f7d3dc5991589e8cf1a591e800afe4f610
uellenberg <JonahUellenberg@hotmail.com>
2022-03-26 15:45:32 -0700

    Bump package versions

--- public/sw.js
+++ public/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-9a8b0a38'], (function (workbox) { 'use strict';
+define(['./workbox-43e9f706'], (function (workbox) { 'use strict';
 
   /**
   * Welcome to your Workbox-powered service worker!
